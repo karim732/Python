4d1d027265db841ac3145f970ba59fda08abb8e9
Shaik Karimulla <karimulla.s@prodapt.com>
2023-08-14 16:54:35 +0530

    else after while

--- while_else.py
+++ while_else.py
@@ -1,9 +1,11 @@
+#  Executing else block after while loop
+
 i = 0 
 while i<2:
     print(i)
     i +=1
     # break
-    # 
+    # if break is executed else block is not executed
 else:
     print(i)
     print('Success')
